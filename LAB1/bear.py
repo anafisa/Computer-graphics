e68a2615ac9dc0759f57230d40a3c15f36aa14e5
Anfisa <anfisa_lepekhina5@mail.ru>
2020-02-18 19:52:44 +0300

    feat: baseline

---
+++ LAB1/bear.py
@@ -0,0 +1,16 @@
+import random
+from PIL import Image, ImageDraw #Подключим необходимые библиотеки
+
+image = Image.open("bear.jpg") #Открываем изображение
+draw = ImageDraw.Draw(image) #Создаем инструмент для рисования
+width  = image.size[0] #Определяем ширину 
+height = image.size[1] #Определяем высоту 	
+pix = image.load() #Выгружаем значения пикселей
+for x in range(width):
+        for y in range(height):
+                a = pix[x, y][0]
+                b = pix[x, y][1]
+                c = pix[x, y][2]
+                draw.point((x, y), (x, b, c))
+image.show()
+del draw
